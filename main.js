6c9c941c3a4f440ea2657e54366995a21d46acb1
hensm <m+github@matt.tf>
2018-02-04 20:59:21 +0000

    Prevent infinite loop

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 "use strict";
 
+let should_revert = true;
+
 browser.tabs.onDetached.addListener((tabId, details) => {
-    browser.tabs.move(tabId, {
-        windowId: details.oldWindowId
-      , index: details.oldPosition
-    }).then(([ tab ]) => {
-        browser.tabs.update(tab.id, {
-            active: true
+    if (should_revert) {
+        should_revert = false;
+
+        browser.tabs.move(tabId, {
+            windowId: details.oldWindowId
+          , index: details.oldPosition
+        }).then(([ tab ]) => {
+            return browser.tabs.update(tab.id, {
+                active: true
+            })
+        }).then(() => {
+            should_revert = true;
         });
-    });
+    }
 });
