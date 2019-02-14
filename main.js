7339525af9a76608b1712da856b3994417f707a7
hensm <m+github@matt.tf>
2019-02-14 06:38:14 +0000

    Add selective tab detach/move

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const _ = browser.i18n.getMessage;
+
+
 const pinnedTabs = new Set;
 
 browser.tabs.query({ pinned: true })
@@ -70,3 +73,152 @@ browser.tabs.onDetached.addListener(async (tabId, details) => {
 
     shouldRevert = true;
 });
+
+
+async function moveTabToWindow (tabId, windowId, pinned) {
+    shouldRevert = false;
+
+    await browser.tabs.move(tabId, {
+        windowId
+      , index: pinned ? 0 : -1
+    });
+
+    shouldRevert = true;
+}
+
+async function moveTabToNewWindow (tabId) {
+    shouldRevert = false;
+
+    const newWindow = await browser.windows.create({
+        tabId
+    });
+
+    shouldRevert = true;
+
+    return newWindow;
+}
+
+
+browser.menus.create({
+    id: "moveTabToNewWindow"
+  , title: _("moveTabToNewWindow")
+  , contexts: [ "tab" ]
+});
+
+browser.menus.create({
+    id: "separator"
+  , type: "separator"
+  , contexts: [ "tab" ]
+});
+browser.menus.create({
+    id: "separatorTitle"
+  , title: _("moveTabToWindow")
+  , enabled: false
+  , contexts: [ "tab" ]
+});
+
+browser.menus.onClicked.addListener(async (info, tab) => {
+    switch (info.menuItemId) {
+        case "moveTabToNewWindow": {
+            moveTabToNewWindow(tab.id);
+            break;
+        }
+
+        default: {
+            if (info.menuItemId.startsWith("win-")) {
+                const destinationWindowId = getMenuWindowId(info.menuItemId);
+                await moveTabToWindow(tab.id, destinationWindowId, tab.pinned);
+                await browser.windows.update(destinationWindowId, {
+                    focused: true
+                });
+                await browser.tabs.update(tab.id, {
+                    active: true
+                });
+            }
+        }
+    }
+});
+
+
+function getWindowMenuId (windowId) {
+    return `win-${windowId}`;
+}
+function getMenuWindowId (menuItemId) {
+    return parseInt(menuItemId.slice(4));
+}
+
+let lastMenuInstanceId = 0;
+let nextMenuInstanceId = 1;
+
+const temporaryMenuIds = new Set();
+
+browser.menus.onShown.addListener(async (info, tab) => {
+    const menuInstanceId = nextMenuInstanceId++;
+    lastMenuInstanceId = menuInstanceId;
+
+    const windows = (await browser.windows.getAll({
+            populate: true
+          , windowTypes: [ "normal" ]
+        })).filter(window => window.id !== tab.windowId);
+
+    if (menuInstanceId !== lastMenuInstanceId) {
+        return;
+    }
+
+    browser.menus.update("separator", {
+        visible: !!windows.length
+    });
+    browser.menus.update("separatorTitle", {
+        visible: !!windows.length
+    });
+
+    for (const menuId of temporaryMenuIds) {
+        if (!windows.find(win => getWindowMenuId(win.id) === menuId)) {
+            browser.menus.remove(menuId);
+            temporaryMenuIds.delete(menuId);
+        }
+    }
+
+    for (const win of windows) {
+        const winMenuId = getWindowMenuId(win.id);
+        const newTitle = _("windowTitle", [
+            win.title.length > 25
+                ? `${win.title.slice(0, 35)}...`
+                : win.title
+          , win.tabs.length
+        ])
+
+        if (!temporaryMenuIds.has(winMenuId)) {
+            console.log(win.tabs.length)
+            browser.menus.create({
+                id: winMenuId
+              , contexts: [ "tab" ]
+              , title: newTitle
+            });
+
+            temporaryMenuIds.add(winMenuId);
+        } else {
+            browser.menus.update(winMenuId, {
+                title: newTitle
+            });
+        }
+    }
+
+    browser.menus.refresh();
+});
+
+
+browser.commands.onCommand.addListener(async name => {
+    switch (name) {
+        case "moveTabToNewWindow": {
+            const [ currentTab ] = await browser.tabs.query({
+                currentWindow: true
+              , active: true
+            });
+
+            moveTabToNewWindow(currentTab.id);
+        }
+    }
+});
+
+
